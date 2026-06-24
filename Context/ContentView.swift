a42cfebdea58fb354cf119af3d86248ebc308c4e
davidHarwardt <david.harwardt@icloud.com>
2026-06-24 21:29:30 +0200

    Initial Commit

---
+++ Context/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Context
+//
+//  Created by David Harwardt on 24.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Context/ContextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextApp.swift
+//  Context
+//
+//  Created by David Harwardt on 24.06.26.
+//
+
+import SwiftUI
+
+@main
+struct ContextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
